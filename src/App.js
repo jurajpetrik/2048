917517bdcb381da717d1b13ede5d6e01d5cf61cf
Juraj Petrik <jpe@coinify.com>
2018-08-08 15:35:30 +0200

    Game rules work

--- src/App.js
+++ src/App.js
@@ -24,60 +24,142 @@ class Board extends Component {
   }
 
   _handleKeyDown(event) {
-    const keys = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
     switch(event.key) {
       case 'ArrowUp':
-        this.addNewNumber();
+        this.moveUp();
         break;
       case 'ArrowDown':
-        this.addNewNumber();
+        this.moveDown();
         break;
       case 'ArrowLeft':
         this.moveLeft();
         break;
       case 'ArrowRight':
-        this.addNewNumber();
+        this.moveRight();
         break;
     }
   }
 
   moveLeft() {
-    //first row
-    //first  square
-    //second  square
     const squares = this.state.squares.slice();
-    squares.forEach((value, index) => {
-      if (index % 4 === 0) {
-        //leftmost square, nowhere to move
-        return;
-      }
+    for (let i = 0; i <= 15; i++) {
+      let index = i;
       if (squares[index] !== 0) {
-          debugger;
-          const leftMostIndex = 4 * Math.floor(index / 4);
-          let currentIndex = index;
-          while (currentIndex - 1 >= leftMostIndex) {
-            if (squares[currentIndex - 1] === 0) {
-              // left is empty, move left
-              squares[currentIndex - 1] = squares[currentIndex];
-              squares[currentIndex] = 0;
-              currentIndex--;
-            }
-            else if (squares[currentIndex - 1] === squares[currentIndex]) {
-              // left has same number, merge, stop moving
-              squares[currentIndex - 1] = squares[currentIndex - 1] + squares[currentIndex];
-              squares[currentIndex] = 0;
-              break;
-            }
-            else {
-              // left has different number, stop moving
-              break;
-            }
+        const leftMostIndex = 4 * Math.floor(index / 4);
+        while (index - 1 >= leftMostIndex) {
+          if (squares[index - 1] === 0) {
+            // left is empty, move left
+            squares[index - 1] = squares[index];
+            squares[index] = 0;
+            index--;
+          }
+          else if (squares[index - 1] === squares[index]) {
+            // left has same number, merge, stop moving
+            squares[index - 1] = squares[index - 1] + squares[index];
+            squares[index] = 0;
+            break;
+          }
+          else {
+            // left has different number, stop moving
+            break;
           }
         }
-    });
+      }
+    }
     this.setState({squares});
     this.addNewNumber();
   }
+
+  moveRight() {
+    const squares = this.state.squares.slice();
+    for (let i= 15; i> 0; i--) {
+      let index = i;
+      if (squares[index] !== 0) {
+        const rightMostIndex = 3 + 4 * Math.floor(index / 4);
+        while (index + 1 <= rightMostIndex) {
+          if (squares[index + 1] === 0) {
+            // right is empty, move right
+            squares[index + 1] = squares[index];
+            squares[index] = 0;
+            index++;
+          }
+          else if (squares[index + 1] === squares[index]) {
+            // right has same number, merge, stop moving
+            squares[index + 1] = squares[index + 1] + squares[index];
+            squares[index] = 0;
+            break;
+          }
+          else {
+            // right has different number, stop moving
+            break;
+          }
+        }
+      }
+    }
+    this.setState({squares});
+    this.addNewNumber();
+  }
+
+  moveUp() {
+    const squares = this.state.squares.slice();
+    for (let i= 15; i> 0; i--) {
+      let index = i;
+      if (squares[index] !== 0) {
+        const topMostIndex = index % 4;
+        while (index - 4 >= topMostIndex) {
+          if (squares[index - 4] === 0) {
+            // up is empty, move up
+            squares[index - 4] = squares[index];
+            squares[index] = 0;
+            index -= 4;
+          }
+          else if (squares[index - 4] === squares[index]) {
+            // up has same number, merge, stop moving
+            squares[index - 4] = squares[index - 4] + squares[index];
+            squares[index] = 0;
+            break;
+          }
+          else {
+            // up has different number, stop moving
+            break;
+          }
+        }
+      }
+    }
+    this.setState({squares});
+    this.addNewNumber();
+  }
+
+  moveDown() {
+    const squares = this.state.squares.slice();
+    for (let i= 0; i <= 15; i++) {
+      let index = i;
+      if (squares[index] !== 0) {
+        const bottomMostIndex = 3 * 4 +index % 4;
+        while (index + 4 <= bottomMostIndex) {
+          if (squares[index + 4] === 0) {
+            // down is empty, move down
+            squares[index + 4] = squares[index];
+            squares[index] = 0;
+            index += 4;
+          }
+          else if (squares[index + 4] === squares[index]) {
+            // down has same number, merge, stop moving
+            squares[index + 4] = squares[index + 4] + squares[index];
+            squares[index] = 0;
+            break;
+          }
+          else {
+            // down has different number, stop moving
+            break;
+          }
+        }
+      }
+    }
+    this.setState({squares});
+    this.addNewNumber();
+  }
+
 
   addNewNumber() {
     const emptySquareIndices = this.state.squares.map((val, index) => val === 0 ? index : null).filter(x => x !== null);
