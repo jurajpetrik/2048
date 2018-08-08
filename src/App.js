d15bbf90324b192dabaf031ecfbecb0079b65d3d
Juraj Petrik <jpe@coinify.com>
2018-08-08 04:04:49 +0200

    Board has kind of a concept of the values

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class Board extends Component {
-  renderSquare(value) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(15).fill(0)
+    };
+  }
+
+  renderSquare(index) {
+    const value = this.state.squares[0];
     return value==0 ? <EmptySquare /> : <Square value={value}/>;
   }
 
@@ -12,22 +19,22 @@ class Board extends Component {
       <div className="App">
         <div className="game-container">
           <div className="grid-container">
-              {this.renderSquare(2)}
-              {this.renderSquare(2)}
-              {this.renderSquare(2)}
-              {this.renderSquare(2)}
               {this.renderSquare(0)}
-              {this.renderSquare(0)}
-              {this.renderSquare(0)}
-              {this.renderSquare(0)}
-              {this.renderSquare(0)}
+              {this.renderSquare(1)}
               {this.renderSquare(2)}
-              {this.renderSquare(2)}
-              {this.renderSquare(2)}
-              {this.renderSquare(2)}
-              {this.renderSquare(2)}
-              {this.renderSquare(2)}
-              {this.renderSquare(2)}
+              {this.renderSquare(3)}
+              {this.renderSquare(4)}
+              {this.renderSquare(5)}
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+              {this.renderSquare(8)}
+              {this.renderSquare(9)}
+              {this.renderSquare(10)}
+              {this.renderSquare(11)}
+              {this.renderSquare(12)}
+              {this.renderSquare(13)}
+              {this.renderSquare(14)}
+              {this.renderSquare(15)}
           </div>
         </div>
 
